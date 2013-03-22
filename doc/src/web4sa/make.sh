5b2cb023fc66d18183d8bcd52bab4ca234aa0516
Hans Petter Langtangen <hpl@simula.no>
2013-03-22 22:43:18 +0100

    updates

---
+++ doc/src/web4sa/make.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# Compile document in HTML versions, Flask+Django, Flask only,
+# Django only, Sphinx version, and LaTeX/PDF version.
+
+sh clean.sh
+
+name=web4sa
+doconce format html $name -DTOPIC=Flask+Django --html-style=bloodish
+if [ $? -ne 0 ]; then exit; fi
+cp $name.html ${name}_plain.html
+doconce split_html ${name}_plain
+if [ $? -ne 0 ]; then exit; fi
+
+doconce format html ${name}_flask -DTOPIC=Flask --html-style=vagrant --html-template=html_templates/template_flask.html
+if [ $? -ne 0 ]; then exit; fi
+doconce split_html ${name}_flask
+if [ $? -ne 0 ]; then exit; fi
+
+doconce format html ${name}_django -DTOPIC=Django --html-style=vagrant --html-template=html_templates/template_django.html
+if [ $? -ne 0 ]; then exit; fi
+cp ${name}_django.html tmp.html
+doconce split_html ${name}_django
+if [ $? -ne 0 ]; then exit; fi
+
+doconce format sphinx $name -DTOPIC=Flask+Django
+doconce split_rst $name
+if [ $? -ne 0 ]; then exit; fi
+doconce sphinx_dir author='H. P. Langtangen and A. E. Johansen' $name theme=basicstrap
+if [ $? -ne 0 ]; then exit; fi
+python automake_sphinx.py
+if [ $? -ne 0 ]; then exit; fi
+cd sphinx-rootdir
+sh make_themes.sh cbc bootstrap basicstrap pyramid redcloud
+cd ..
+mv sphinx-rootdir/sphinx-* .
+
+doconce format pdflatex $name -DTOPIC=Flask+Django
+if [ $? -ne 0 ]; then exit; fi
+doconce ptex2tex $name envirs=minted
+pdflatex -shell-escape $name
+makeindex $name
+pdflatex -shell-escape $name
+pdflatex -shell-escape $name
+
+doconce format html index --html-style=bloodish
+
--- src/apps/vib/vib3_flask/compute.py
+++ src/apps/vib/vib3_flask/compute.py
@@ -1,4 +1,4 @@
-from numpy import exp, cos, linspace
+from numpy import exp, cos, linspace, zeros_like
 import matplotlib.pyplot as plt
 import os, time, glob, math
 
@@ -24,10 +24,19 @@ def compute_vib(A=1, b=0.2, w=2*math.pi, T=5, resolution=500):
     plt.savefig(plotfile)
     return plotfile
 
-def gamma_pdf(x, a, h, A):
+def gamma_density(x, a, h, A):
     # http://en.wikipedia.org/wiki/Gamma_distribution
     xA = x/float(A)
     return abs(h)/(math.gamma(a)*A)*(xA)**(a*h-1)*exp(-xA**h)
+
+def gamma_cumulative(x, a, h, A):
+    # Integrate gamma_density using the Trapezoidal rule.
+    # Assume x is array.
+    g = gamma_density(x, a, h, A)
+    r = zeros_like(x)
+    for i in range(len(r)-1):
+        r[i+1] = r[i] + 0.5*(g[i] + g[i+1])*(x[i+1] - x[i])
+    return r
 
 def compute_gamma(a=0.5, h=2, A=math.sqrt(2), resolution=500):
     """Return plot and mean/st.dev. value of the gamma density."""
@@ -36,7 +45,7 @@ def compute_gamma(a=0.5, h=2, A=math.sqrt(2), resolution=500):
     stdev = A/math.gamma(a)*math.sqrt(
         math.gamma(a + 2./h)*math.gamma(a) - gah**2)
     x = linspace(0, 7*stdev, resolution+1)
-    y = gamma_pdf(x, a, h, A)
+    y = gamma_density(x, a, h, A)
     plt.figure()  # needed to avoid adding curves in plot
     plt.plot(x, y)
     plt.title('a=%g, h=%g, A=%g' % (a, h, A))
@@ -48,9 +57,16 @@ def compute_gamma(a=0.5, h=2, A=math.sqrt(2), resolution=500):
             os.remove(filename)
     # Use time since Jan 1, 1970 in filename in order make
     # a unique filename that the browser has not chached
-    plotfile = os.path.join('static', str(time.time()) + '.png')
-    plt.savefig(plotfile)
-    return plotfile, mean, stdev
+    t = str(time.time())
+    plotfile1 = os.path.join('static', 'density_%s.png' % t)
+    plotfile2 = os.path.join('static', 'cumulative_%s.png' % t)
+    plt.savefig(plotfile1)
+    y = gamma_cumulative(x, a, h, A)
+    plt.figure()
+    plt.plot(x, y)
+    plt.grid(True)
+    plt.savefig(plotfile2)
+    return plotfile1, plotfile2, mean, stdev
 
 if __name__ == '__main__':
     print compute_vib()
