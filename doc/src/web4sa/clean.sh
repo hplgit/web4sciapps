924194d7dd62b5d843741bc65b52929086b69f7b
Hans Petter Langtangen <hpl@simula.no>
2013-04-05 01:39:39 +0200

    updates

--- doc/src/web4sa/clean.sh
+++ doc/src/web4sa/clean.sh
@@ -2,3 +2,7 @@
 name=web4sa
 doconce clean
 rm -rf ${name}_*.html sphinx-* automake_sphinx.py
+rm -rf src-${name}/apps/vib/vib1_flask/static/*.png
+rm -rf src-${name}/apps/vib/vib3_flask/static
+rm -rf src-${name}/apps/vib/vib1_django/static
+rm -rf src-${name}/apps/vib/vib2_django/static
