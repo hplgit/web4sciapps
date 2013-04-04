f903e4c0ede9c51718f78caf9441c87c2428e127
Hans Petter Langtangen <hpl@simula.no>
2013-04-05 01:30:48 +0200

    updates

---
+++ doc/src/web4sa/src-web4sa/apps/vib/vib4_flask/compute.py
@@ -0,0 +1,60 @@
+from numpy import exp, cos, linspace, zeros_like
+import matplotlib.pyplot as plt
+import os, time, glob, math
+
+def gamma_density(x, a, h, A):
+    # http://en.wikipedia.org/wiki/Gamma_distribution
+    xA = x/float(A)
+    return abs(h)/(math.gamma(a)*A)*(xA)**(a*h-1)*exp(-xA**h)
+
+def gamma_cumulative(x, a, h, A):
+    # Integrate gamma_density using the Trapezoidal rule.
+    # Assume x is array.
+    g = gamma_density(x, a, h, A)
+    r = zeros_like(x)
+    for i in range(len(r)-1):
+        r[i+1] = r[i] + 0.5*(g[i] + g[i+1])*(x[i+1] - x[i])
+    return r
+
+def compute_gamma(a=0.5, h=2.0, A=math.sqrt(2), resolution=500):
+    """Return plot and mean/st.dev. value of the gamma density."""
+    gah = math.gamma(a + 1./h)
+    mean = A*gah/math.gamma(a)
+    stdev = A/math.gamma(a)*math.sqrt(
+        math.gamma(a + 2./h)*math.gamma(a) - gah**2)
+    x = linspace(0, 7*stdev, resolution+1)
+    y = gamma_density(x, a, h, A)
+    plt.figure()  # needed to avoid adding curves in plot
+    plt.plot(x, y)
+    plt.title('a=%g, h=%g, A=%g' % (a, h, A))
+    # Make Matplotlib write to StringIO file object and grab
+    # return the object's string
+    from StringIO import StringIO
+    figfile = StringIO()
+    plt.savefig(figfile, format='png')
+    figfile.seek(0)  # rewind to beginning of file
+    import base64
+    figdata_density_png = base64.b64encode(figfile.buf)
+    figfile = StringIO()
+    plt.savefig(figfile, format='svg')
+    figfile.seek(0)
+    figdata_density_svg = '<svg' + figfile.buf.split('<svg')[1]
+
+    y = gamma_cumulative(x, a, h, A)
+    plt.figure()
+    plt.plot(x, y)
+    plt.grid(True)
+    figfile = StringIO()
+    plt.savefig(figfile, format='png')
+    figfile.seek(0)
+    figdata_cumulative_png = base64.b64encode(figfile.buf)
+    figfile = StringIO()
+    plt.savefig(figfile, format='svg')
+    figfile.seek(0)
+    figdata_cumulative_svg = '<svg' + figfile.buf.split('<svg')[1]
+    return figdata_density_png, figdata_cumulative_png, \
+           figdata_density_svg, figdata_cumulative_svg, \
+           mean, stdev
+
+if __name__ == '__main__':
+    print compute_vib()
---
+++ doc/src/web4sa/src-web4sa/apps/vib/vib4_flask/view.py
@@ -0,0 +1,28 @@
+from model import InputForm
+from flask import Flask, render_template, request
+from compute import compute_gamma as compute
+import sys, os, inspect
+
+app = Flask(__name__)
+
+@app.route('/vib4', methods=['GET', 'POST'])
+def index():
+    form = InputForm(request.form)
+    if request.method == 'POST' and form.validate():
+        arg_names = inspect.getargspec(compute).args
+        kwargs = {name: getattr(form, name).data
+                  for name in arg_names if hasattr(form, name)}
+        result = compute(**kwargs)
+    else:
+        result = None
+    # Concatenate view_forms.html and view_results.html
+    forms_html   = os.path.join('templates', 'view_forms.html')
+    results_html = os.path.join('templates', 'view_results.html')
+    view_html    = os.path.join('templates', 'view.html')
+    f_forms = open(forms_html, 'r')
+    f_res   = open(results_html, 'r')
+    f_view  = open(view_html, 'w')
+    f_view.write(f_forms.read() + f_res.read())
+    f_forms.close();  f_res.close();  f_view.close()
+    return render_template(os.path.basename(view_html),
+                           form=form, result=result)
