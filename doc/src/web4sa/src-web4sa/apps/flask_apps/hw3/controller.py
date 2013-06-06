4a7c27ac223dc64d429e7cc36dcd337718d88e90
Hans Petter Langtangen <hpl@simula.no>
2013-06-06 14:04:43 +0200

    Dropped view.py for flask apps.

--- doc/src/web4sa/src-web4sa/apps/flask_apps/hw3/controller.py
+++ doc/src/web4sa/src-web4sa/apps/flask_apps/hw3/controller.py
@@ -1,4 +1,19 @@
-from view import app
+from flask import Flask, render_template, request
+from compute import compute
+
+app = Flask(__name__)
+
+@app.route('/hw3', methods=['GET', 'POST'])
+def index():
+    form = InputForm(request.form)
+    if request.method == 'POST' and form.validate():
+        r = form.r.data
+        s = compute(r)
+    else:
+        s = None
+
+    return render_template("view.html", form=form, s=s)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- doc/src/web4sa/src-web4sa/apps/flask_apps/vib2/controller.py
+++ doc/src/web4sa/src-web4sa/apps/flask_apps/vib2/controller.py
@@ -1,4 +1,21 @@
-from view import app
+from flask import Flask, render_template, request
+from model import InputForm
+from compute import compute
+
+app = Flask(__name__)
+
+@app.route('/vib2', methods=['GET', 'POST'])
+def index():
+    form = InputForm(request.form)
+    if request.method == 'POST' and form.validate():
+        for field in form:
+            # Make local variable (name field.name)
+            exec('%s = %s' % (field.name, field.data))
+        result = compute(A, b, w, T)
+    else:
+        result = None
+
+    return render_template('view.html', form=form, result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
