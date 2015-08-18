d07db57dde59f2b75e927db89ae88ee9f12daeb4
Hans Petter Langtangen <hpl@simula.no>
2015-08-18 15:16:33 -0700

    Updated Bokeh instructions to use script and div components.

--- doc/src/web4sa/src-web4sa/apps/flask_apps/vib3/compute.py
+++ doc/src/web4sa/src-web4sa/apps/flask_apps/vib3/compute.py
@@ -10,10 +10,6 @@ def compute(A, b, w, T, resolution=500):
     t = linspace(0, T, resolution+1)
     u = damped_vibrations(t, A, b, w)
 
-    # output to static HTML file
-    outname = 'tmp.html'
-    plt.output_file(outname, title="Damped vibrations")
-
     # create a new plot with a title and axis labels
     TOOLS = "pan,wheel_zoom,box_zoom,reset,save,box_select,lasso_select"
     p = plt.figure(title="simple line example", tools=TOOLS,
@@ -22,21 +18,21 @@ def compute(A, b, w, T, resolution=500):
     # add a line renderer with legend and line thickness
     p.line(t, u, legend="u(t)", line_width=2)
 
-    # show the results
-    #plt.show(p)
-    plt.save(p)
-    with  open(outname, 'r') as f:
-        text = f.read()
-    # Grab head and body
-    m = re.search('<head>(.+)</head>.+<body>(.+)</body>', text,
-                  flags=re.DOTALL)
-    if m:
-        head = m.group(1)
-        body = m.group(2)
-    else:
-        raise ValueError('Could not parse the bokeh output in tmp.html')
-    #os.remove(outname)
-    return head, body
+    from bokeh.resources import CDN
+    from bokeh.embed import components
+    script, div = components(p)
+    head = """
+<link rel="stylesheet"
+ href="http://cdn.pydata.org/bokeh/release/bokeh-0.9.0.min.css"
+ type="text/css" />
+<script type="text/javascript"
+ src="http://cdn.pydata.org/bokeh/release/bokeh-0.9.0.min.js">
+</script>
+<script type="text/javascript">
+Bokeh.set_log_level("info");
+</script>
+"""
+    return head, script, div
 
 if __name__ == '__main__':
     print compute(A=1, b=0.2, w=6.28, T=4, resolution=500)
